7777a7b49d7e519aa77787442cf192a2b5fc398b
asmaSmida <asmasmida@insat.u-carthage.tn>
2023-02-18 02:28:36 +0100

    first

---
+++ LSP/src/com/directi/training/lsp/exercise_refactored/ElectronicDuck.java
@@ -0,0 +1,32 @@
+package com.directi.training.lsp.exercise_refactored;
+
+public class ElectronicDuck implements IDuck {
+    private boolean _on = false;
+
+    @Override
+    public void quack() throws IDuckException {
+        if (_on) {
+            System.out.println("Electronic duck quack...");
+        } else {
+            throw new IDuckException("Can't quack when off");
+        }
+    }
+
+    @Override
+    public void swim() throws IDuckException {
+        if (_on) {
+            System.out.println("Electronic duck swim...");
+        } else {
+            throw new IDuckException("Cant swim when off");
+        }
+    }
+
+    public void turnOn() {
+        this._on = true;
+    }
+
+    public void turnOff() {
+        this._on = false;
+    }
+
+}
